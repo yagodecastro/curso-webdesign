fa239f5b58254078887436c3ba4a7d59e7053a38
yagodecastro <yagofernandodecastro@gmail.com>
2023-10-17 09:12:15 -0300

    ex 26

---
+++ exercicios/exercicio-026/scripts/main.js
@@ -0,0 +1,11 @@
+// Sistema Webdesign
+// Armazenar a lista de alunos do curso de Webdesign e mostrar na tela.
+
+// Quais são os dados?
+// Alunos: Nome ()
+
+// Quais são as ações/funções/métodos/etapas/passos
+
+// 1. Pegar os dados do usuário ()
+// 1.2. Salvar os dados do usuário ()
+// 3. Exibir os dados na tela ()
